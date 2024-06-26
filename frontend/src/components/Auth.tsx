e90dd5cad16a5b5f9b58f26f1358271369c0ced1
Siraddeen <sircoil17@gmail.com>
2024-06-26 16:59:54 +0530

    modified

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,6 +1,6 @@
-import { SigninInputs, SignupInputs } from "@ronsen/w13";
-import React, { ChangeEvent, useState } from "react";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { SignupInputs } from "@ronsen/w13";
+import  { ChangeEvent, useState } from "react";
+import { Link,  useNavigate } from "react-router-dom";
 import axios from "axios";
 import { BACKEND_URL } from "../config";
 
